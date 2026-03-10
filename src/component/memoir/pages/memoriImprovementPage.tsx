72cc81c7f55ba302ff9b25feb7af7d497b2c93ad
Younggwan <kimyogun78@naver.com>
2026-03-10 19:47:27 +0900

    #41 Feat: MemoirImprovement페이지 구현

--- src/component/memoir/pages/memoriImprovementPage.tsx
+++ src/component/memoir/pages/memoriImprovementPage.tsx
@@ -10,7 +10,20 @@ const MemoirImprovementPage = () => {
         <section className={"improvement-section"}>
             <h1 className={"memoir-improvement-title"}>느낀점이나 개선점이 있나요?</h1>
             <span>이런 상황은 어떻게 해결할 수 있을까요 편하게 이야기해주세요</span>
-            <MemoirTitleList editable={false} width={"625px"} />
+            <div className={"improvement-box"}>
+                <div className={"left-box"}>
+                    <div className="memoir-title">
+                        <span>오늘 내게 있었던 일</span>
+                    </div>
+                    <MemoirTitleList editable={false} width={"625px"} />
+                </div>
+                <div className={"right-box"}>
+                    <div className="memoir-title">
+                        <span>개선점</span>
+                    </div>
+                    <textarea placeholder="개선점을 입력해주세요" className={"improvement-editor"} />
+                </div>
+            </div>
 
             <div className="progress-circles">
                 <div className="first-circle"></div>
@@ -19,7 +32,7 @@ const MemoirImprovementPage = () => {
 
             <div className={"btn-footer"}>
                 <Link to="/memoir/write">
-                    <button className={"feedback-btn"}>이전({feedbackCnt}/5)</button>
+                    <button className={"feedback-btn"}>이전</button>
                 </Link>
                 <Link to="/memoir/feedback">
                     <button className={"feedback-btn"}>피드백 받기({feedbackCnt}/5)</button>

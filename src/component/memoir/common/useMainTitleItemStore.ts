714be7aa3ccbb5494e5eb0adf2f111d72ec1d3d8
Younggwan <kimyogun78@naver.com>
2026-03-15 00:21:00 +0900

    #49 Feat: MemoirTypes Mode속성 추가

--- src/component/memoir/common/useMainTitleItemStore.ts
+++ src/component/memoir/common/useMainTitleItemStore.ts
@@ -5,20 +5,25 @@ import type { MainTitleItem } from "./memoir.types";
 const data: MainTitleItem[] = [
     {
         title: "2026/01/01 회고 - 오늘 공부한 것",
+        mode: "VIEW",
         subMemoirTitles: [
             {
                 title: "TSX란?",
+                mode: "VIEW",
             },
         ],
     },
     {
         title: "2026/03/09 회고 - 오늘의 나의 일기",
+        mode: "VIEW",
         subMemoirTitles: [
             {
                 title: "문구점을 갔다.",
+                mode: "VIEW",
             },
             {
                 title: "산책을 갔다.",
+                mode: "VIEW",
             },
         ],
     },
@@ -29,6 +34,10 @@ type MainTitleItemStore = {
     addSubTitle: (mainTitleId: number, newSubTitle: string) => void;
     addMainTitle: (mainTitle: string) => void;
     setMainTitleItems: (mainTitleItems: MainTitleItem[]) => void;
+    deleteMainTitleItem: (id: number) => void;
+    updateMainTitleMode: (id: number) => void;
+    deleteSubTitleItem: (mainTitleId: number, id: number) => void;
+    updateSubTitleMode: (mainTitleId: number, id: number) => void;
 };
 
 export const useMainTitleItemStore = create<MainTitleItemStore>()(
@@ -36,12 +45,33 @@ export const useMainTitleItemStore = create<MainTitleItemStore>()(
         mainTitleItems: data,
         addSubTitle: (mainTitleId, newSubTitle) =>
             set((state) => {
-                state.mainTitleItems[mainTitleId].subMemoirTitles.push({ title: newSubTitle });
+                state.mainTitleItems[mainTitleId].subMemoirTitles.push({ title: newSubTitle, mode: "VIEW" });
             }),
         addMainTitle: (mainTitle: string) =>
             set((state) => {
-                state.mainTitleItems.push({ title: mainTitle, subMemoirTitles: [] });
+                state.mainTitleItems.push({ title: mainTitle, mode: "VIEW", subMemoirTitles: [] });
             }),
         setMainTitleItems: (mainTitleItems: MainTitleItem[]) => set({ mainTitleItems }),
+        deleteMainTitleItem: (mainTitleId: number) =>
+            set((state) => {
+                state.mainTitleItems.splice(mainTitleId, 1);
+            }),
+        updateMainTitleMode: (mainTitleId: number) => {
+            set((state) => {
+                const mode = state.mainTitleItems[mainTitleId].mode;
+                state.mainTitleItems[mainTitleId].mode = mode === "VIEW" ? "EDIT" : "VIEW";
+            });
+        },
+        deleteSubTitleItem: (mainTitleId: number, subTitleId: number) => {
+            set((state) => {
+                state.mainTitleItems[mainTitleId].subMemoirTitles.splice(subTitleId, 1);
+            });
+        },
+        updateSubTitleMode: (mainTitleId: number, subTitleId: number) => {
+            set((state) => {
+                const subTitleItem = state.mainTitleItems[mainTitleId].subMemoirTitles[subTitleId];
+                subTitleItem.mode = subTitleItem.mode === "VIEW" ? "EDIT" : "VIEW";
+            });
+        },
     })),
 );
